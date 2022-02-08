20ddbb745fc5449b47aad94002b0ea666581b814
Preston Harms <prestonharms5@gmail.com>
2022-02-07 21:04:58 -0800

    02-07-22
    Completed Project, ready for testing

--- mac_addr_changer/main.py
+++ mac_addr_changer/main.py
@@ -8,19 +8,70 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
+import re
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    subprocess.call("ifconfig eth0 up", shell=True)
+def get_args():
+        parser = optparse.OptionParser()
+
+        parser.add_option("-i","--interface", dest="interface", help = "Interface to help change your MAC Address")
+        parser.add_option("-m","--mac", dest="new_mac", help = "New Mac Address") 
+
+        (options, arguments) = parser.parse_args()
+
+        if not options.interface:
+            parser.error("[-] Please specify an interface, use --help for more info")
+        if not options.new_mac:
+            options.new_mac = "11:22:33:44:55:66"
+        
+
+def change_mac(interface, mac_addr):
+        # Use a breakpoint in the code line below to debug your script.
     subprocess.call("ifconfig", shell=True)
 
-    # subprocess.call("ifconfig eth0 down", shell=True)
-    # subprocess.call("ifconfig hw ether 11:22:33:44:55:66", shell=True)
-    # subprocess.call("ifconfig eth0 up", shell=True)
+    call_set_interface_down = "fconfig " + interface + " down"
+    call_set_interface_up = "fconfig " + interface + " up"
+    call_set_mac_addr = "ifconfig " + interface + "hw ether " +mac_addr
 
+    print(call_set_interface_down)
+    # subprocess.call(call_set_interface_down, shell=True)
+    subprocess.call(["ifconfig", interface, "down"])
+    print(call_set_mac_addr)
+    # subprocess.call(call_set_mac_addr, shell=True)
+    subprocess.call(["ifconfig", interface, "hw", "ether", mac_addr])
+    print(call_set_interface_up)
+    # subprocess.call(call_set_interface_up, shell=True)
+    subprocess.call(["ifconfig", interface, "down"])
+
+    # subprocess.call("ifconfig", shell=True)
+
+
+def verify_mac_change(interface, mac_addr):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_addr_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    mac_addr_result = mac_addr_search_result.group(0)
+
+    if mac_addr_result == mac_addr:
+        print("Interface changed successsfully")
+    else:
+        print("Interface " + interface + " not changed successsfully, value is " + mac_addr_result)
+
+
+
+def mac_changer():
+    options = get_args()
+
+    # interface = input("Please enter the interface you would like to change: ")
+    interface = options.interface
+    # mac_addr = input("Please enter the new mac address for " + interface + ":")
+    mac_addr = options.new_mac
+
+    change_mac(interface, mac_addr)
+
+    verify_mac_change(interface, mac_addr)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Preston')
+    mac_changer()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
